f30973a0f567524c133a5b1cd425fe8a5bdb02c5
stepandamrin <91244185+stepandamrin@users.noreply.github.com>
2021-09-23 17:32:48 +0300

    Initial commit

---
+++ src/main/java/ru/damrin/bmp/BmpApplication.java
@@ -0,0 +1,13 @@
+package ru.damrin.bmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmpApplication.class, args);
+    }
+
+}
