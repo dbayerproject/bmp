5797f341cf7fcff738137aca503db6ba3d18dd17
a <a>
2021-10-04 19:49:21 +0500

    fix: SecurityConfig.class

--- src/main/java/ru/damrin/bmp/config/SecurityConfig.java
+++ src/main/java/ru/damrin/bmp/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package ru.damrin.bmp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -14,11 +13,6 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-    }
 
     @Bean
     @Override
