66bd082c139fd4817c7e3847ebc63c3528fdcaa4
Gorbunkovaya <dgorbunkovaya@gmail.com>
2021-10-14 15:30:29 +0300

    Set product

---
+++ src/main/java/ru/damrin/bmp/model/Product.java
@@ -0,0 +1,137 @@
+package ru.damrin.bmp.model;
+
+import ru.damrin.bmp.dto.ProductDTO;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Integer id;
+    private String name;
+    private String description;
+    private String sku;
+    private Double price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private Category category;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "discount_id", referencedColumnName = "id")
+    private Discount discount;
+    @ManyToOne
+    @JoinColumn(name = "inventory_id", referencedColumnName = "id")
+    private Inventory inventory;
+
+
+    public Product() {
+
+    }
+
+    public Product(Integer id, String name, String description, String sku, Double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.sku = sku;
+        this.price = price;
+    }
+
+    public Product(Integer id, String name, String description, String sku, Double price, Category category, Discount discount, Inventory inventory) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.sku = sku;
+        this.price = price;
+        this.category = category;
+        this.discount = discount;
+        this.inventory = inventory;
+    }
+
+    public Product(String name, String description, String sku, Double price) {
+        this.name = name;
+        this.description = description;
+        this.sku = sku;
+        this.price = price;
+    }
+
+    public ProductDTO toDTO() {
+        return new ProductDTO(id, name, description, sku, price);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Discount getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Discount discount) {
+        this.discount = discount;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return description;
+    }
+
+    public void setDesc(String description) {
+        this.description = description;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+}
