aa5750e2a6cfa365579958d5393d1db33f425ea2
prod.god <prod.god666@gmail.com>
2021-10-29 21:28:31 +0300

    fix_authorization 2.0

--- src/main/java/ru/damrin/bmp/controllers/WebController.java
+++ src/main/java/ru/damrin/bmp/controllers/WebController.java
@@ -1,23 +1,52 @@
 package ru.damrin.bmp.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-//Через конфиги редиректить не хочет. пришлось слепить контроллер.
+import org.springframework.web.bind.annotation.RequestBody;
+import ru.damrin.bmp.domain.Role;
+import ru.damrin.bmp.domain.User;
+import ru.damrin.bmp.dto.UserDTO;
+import ru.damrin.bmp.service.interfaces.RoleService;
+import ru.damrin.bmp.service.interfaces.UserService;
 
-@RestController
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+@Controller
 public class WebController {
 
 
-    @GetMapping("/hello")
-    public ResponseEntity<String> helloAdmin() {
-        String body = "Hello, Admin";
-        return ResponseEntity.ok().body(body);
+    private final RoleService roleService;
+    private final UserService userService;
+    private  final PasswordEncoder passwordEncoder;
+
+    public WebController(RoleService roleService, UserService userService, PasswordEncoder passwordEncoder) {
+        this.roleService = roleService;
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping("/hellouser")
-    public ResponseEntity<String> helloUser() {
-        String body = "Hello, User";
-        return ResponseEntity.ok().body(body);
+    @GetMapping("/login")
+    public String helloAdmin() {
+
+        if (roleService.findAll().isEmpty()) {
+            roleService.add(new Role("ROLE_ADMIN"));
+        }
+        Set<Role> adminRoles = Stream.of(roleService.getByName("ROLE_ADMIN")).collect(Collectors.toSet());
+        User admin = new User("admin", "admin", "admin@admin.com", "Admin", "Adminov", "88002005555", adminRoles);
+        admin.setPassword(passwordEncoder.encode(admin.getPassword()));
+        userService.saveUser(admin);
+        return "login";
+    }
+
+    @GetMapping("/registration")
+    public String helloUser(Model model, UserDTO userForm) {
+        model.addAttribute("userForm", userForm);
+        return "registration";
     }
 }
