d8939d11e9a3541bfad6d84e9721a73931d49934
prod.god <prod.god666@gmail.com>
2021-10-30 08:42:05 +0300

    fix_karate from auth

--- src/main/java/ru/damrin/bmp/exceptions/UserNotFoundException.java
+++ src/main/java/ru/damrin/bmp/exceptions/UserNotFoundException.java
@@ -1,7 +1,9 @@
+
 package ru.damrin.bmp.exceptions;
 
 public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(int id) {
         super("Could not find user with the id " + id);
     }
+<<<<<<< HEAD
 }
