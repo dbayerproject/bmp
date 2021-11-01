cd3946cdb4c744f7110eb4df611f43fdb6f3e099
prod.god <prod.god666@gmail.com>
2021-11-01 20:35:35 +0300

    fix

--- src/main/java/ru/damrin/bmp/exceptions/UserNotFoundException.java
+++ src/main/java/ru/damrin/bmp/exceptions/UserNotFoundException.java
@@ -5,5 +5,5 @@ public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(int id) {
         super("Could not find user with the id " + id);
     }
-<<<<<<< HEAD
+
 }
