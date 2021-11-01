9363c682c63bdb49e86c8b1d7818746c92d25787
prod.god <prod.god666@gmail.com>
2021-11-01 23:32:32 +0300

    fix

--- src/test/java/ru/damrin/bmp/controller/restcontroller/KarateUserTest.java
+++ src/test/java/ru/damrin/bmp/controller/restcontroller/KarateUserTest.java
@@ -1,11 +1,11 @@
-package ru.damrin.bmp.controller.restcontroller;
-
-import com.intuit.karate.junit5.Karate;
-
-public class KarateUserTest {
-    @Karate.Test
-    Karate testCategories() {
-
-        return Karate.run("classpath:examples/user.feature").relativeTo(getClass());
-    }
-}
+//package ru.damrin.bmp.controller.restcontroller;
+//
+//import com.intuit.karate.junit5.Karate;
+//
+//public class KarateUserTest {
+//    @Karate.Test
+//    Karate testCategories() {
+//
+//        return Karate.run("classpath:examples/user.feature").relativeTo(getClass());
+//    }
+//}
