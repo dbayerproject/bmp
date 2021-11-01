13db449fdc1241da5226408c273c531058e5a40c
prod.god <prod.god666@gmail.com>
2021-11-02 00:12:32 +0300

    fix_last

--- src/main/java/ru/damrin/bmp/domain/User.java
+++ src/main/java/ru/damrin/bmp/domain/User.java
@@ -19,7 +19,7 @@ public class User extends BaseTimeEntity implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
-    @Column(name = "username")
+    @Column(name = "username", unique = true)
     private String username;
     @Column(name = "password")
     private String password;
